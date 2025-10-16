9509fcb858e9aa51f91a6df48c6bebf1e4196a00
Faizan Ahmed <72260047+FaizanAhmed-RDVC1567@users.noreply.github.com>
2025-10-16 17:08:57 +0400

    Create bubble_sort_improved.c

---
+++ bubble_sort_improved.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+	int my_arr[] = {717, 880, 394, 297, 999, 614, 508, 102, 450};
+	int arr_len = sizeof(my_arr) / sizeof(my_arr[0]);
+
+	for(int i = 0; i < arr_len-1; i++) {
+    	bool elemSwapped = false;
+		for(int j = 0; j < arr_len-i-1; j++} {
+			if(my_arr[j] > my_arr[j+1]) {
+				int temp = my_arr[j];
+				my_arr[j] = my_arr[j+1];
+				my_arr[j+1] = temp;
+        		elemSwapped = true;
+			}
+		}
+    	if(!elemSwapped) {
+      		break;
+    	}
+	}
+
+	printf("Sorted array: ");
+	for(int i = 0; i < arr_len; i++) {
+		printf("%d", my_arr[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
